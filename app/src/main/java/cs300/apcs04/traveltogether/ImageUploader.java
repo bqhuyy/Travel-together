082e86345d84ca69db810f9ddc9dc94355b3ffd9
ptq204 <ptquyen1998@gmail.com>
2018-10-25 15:39:34 +0700

    Change backgroud

---
+++ app/src/main/java/cs300/apcs04/traveltogether/ImageUploader.java
@@ -0,0 +1,103 @@
+package cs300.apcs04.traveltogether;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImageUploader extends AsyncTask<Bitmap, Void, String> {
+
+	private ProgressDialog mProgressDialog;
+	private ByteArrayOutputStream mbos;
+	private byte[] mBitmapdata;
+	private String mImgdata;
+	private Context mContext;
+	private Handler mHandler;
+	private static final String URLAPI = "https://us-central1-serverapi-57ed2.cloudfunctions.net/processImage";
+	public ImageUploader(Context context){
+
+		mContext = context;
+		mHandler = new Handler(context.getMainLooper());
+		mProgressDialog = new ProgressDialog(context);
+	}
+
+	private void comPressBitmap(Bitmap mBitmap){
+		mbos = new ByteArrayOutputStream();
+		mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, mbos);
+		mBitmapdata = mbos.toByteArray();
+		mImgdata = Base64.encodeToString(mBitmapdata, Base64.DEFAULT);
+	}
+
+	@Override
+	protected String doInBackground(Bitmap... bitmaps) {
+
+		Bitmap mBitmap = bitmaps[0];
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				mHandler.post(new Runnable() {
+					@Override
+					public void run() {
+						mProgressDialog.setMessage("Uploading, please wait...");
+						mProgressDialog.show();
+					}
+				});
+			}
+		}).start();
+
+		comPressBitmap(mBitmap);
+
+		//sending image to server
+		StringRequest request = new StringRequest(Request.Method.POST, URLAPI, new Response.Listener<String>(){
+			@Override
+			public void onResponse(String s) {
+				mProgressDialog.dismiss();
+				Toast.makeText(mContext, "Uploaded successfully " + s, Toast.LENGTH_LONG).show();
+				/*Intent intent = new Intent(mContext, PlaceInfoActivity.class);
+				intent.putExtra("placeID", s);
+				startActivity(intent);*/
+			}
+		},new Response.ErrorListener(){
+			@Override
+			public void onErrorResponse(VolleyError volleyError) {
+				mProgressDialog.dismiss();
+				Toast.makeText(mContext, "Some error occurred -> "+volleyError, Toast.LENGTH_LONG).show();
+			}
+		}) {
+			//adding parameters to send
+			@Override
+			protected Map<String, String> getParams() throws AuthFailureError {
+				Map<String, String> parameters = new HashMap<String, String>();
+				parameters.put("img", mImgdata);
+				return parameters;
+			}
+		};
+
+		RequestQueue rQueue = Volley.newRequestQueue(mContext);
+		rQueue.add(request);
+		Integer tmp = mImgdata.length();
+		Log.d("quyen", String.valueOf(tmp));
+		return mImgdata;
+	}
+
+	@Override
+	protected void onPostExecute(String s) {
+		super.onPostExecute(s);
+	}
+}
--- app/src/main/java/cs300/apcs04/traveltogether/MainActivity.java
+++ app/src/main/java/cs300/apcs04/traveltogether/MainActivity.java
@@ -1,13 +1,79 @@
 package cs300.apcs04.traveltogether;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton mButtonLoadImage;
+    private LinearLayout mLayoutChooseImage;
+    private Bitmap mBitmap;
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    private ImageView mCaptureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+		mLayoutChooseImage.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				chooseImageFromDevice();
+			}
+		});
     }
+
+	public void init(){
+		mButtonLoadImage = (ImageButton) findViewById(R.id.buttonLoadPicture);
+		mLayoutChooseImage = (LinearLayout) findViewById(R.id.layout_chooseImg);
+		mCaptureView = (ImageView) findViewById(R.id.LoadedImage);
+	}
+
+	public void chooseImageFromDevice(){
+		Intent itent = new Intent(Intent.ACTION_GET_CONTENT);
+		itent.setType("image/*");
+
+		Intent pickitent = new Intent(
+				Intent.ACTION_PICK,
+				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		pickitent.setType("image/*");
+
+		Intent chooseitent = Intent.createChooser(itent, "Select image");
+		chooseitent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickitent});
+
+		startActivityForResult(chooseitent, RESULT_LOAD_IMAGE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+			Log.d("123", "success");
+			Uri imguri = data.getData();
+			mCaptureView.setImageURI(imguri);
+			try {
+				mBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imguri);
+				new ImageUploader(this).execute(mBitmap);
+			}catch(IOException ioe){
+				Log.d("error", "Cannot choose image");
+			}
+
+		}
+	}
 }
