6e736215cc03b07fdaf79be8a9a31fadbd72073b
ldtuanAPCS <ldtuan@apcs.vn>
2018-10-28 11:40:08 +0700

    initial noteapp

---
+++ app/src/main/java/cs300/apcs04/traveltogether/Note_MainActivity.java
@@ -0,0 +1,16 @@
+package cs300.apcs04.traveltogether;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Note_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_main);
+    }
+
+
+}
