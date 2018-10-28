656076ff4d50a24b9023137bfd3718074e3a2006
ldtuanAPCS <ldtuan@apcs.vn>
2018-10-28 11:40:08 +0700

    initial noteapp

--- app/src/main/java/cs300/apcs04/traveltogether/MainActivity.java
+++ app/src/main/java/cs300/apcs04/traveltogether/MainActivity.java
@@ -1,13 +1,26 @@
 package cs300.apcs04.traveltogether;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_Note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_Note = findViewById(R.id.Note_btn);
+        btn_Note.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent noteIntent = new Intent(MainActivity.this, Note_MainActivity.class );
+                startActivity(noteIntent);
+            }
+        });
     }
 }
