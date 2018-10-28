5702f981cc7d151c4513cb4f466bf9ecc20e68e5
ldtuanAPCS <ldtuan@apcs.vn>
2018-10-28 12:34:14 +0700

    add activity_add_note.xml

---
+++ app/src/main/java/cs300/apcs04/traveltogether/Note_AddNote_Activity.java
@@ -0,0 +1,13 @@
+package cs300.apcs04.traveltogether;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Note_AddNote_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+}
--- app/src/main/java/cs300/apcs04/traveltogether/Note_Main_Activity.java
+++ app/src/main/java/cs300/apcs04/traveltogether/Note_Main_Activity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class Note_MainActivity extends AppCompatActivity {
+public class Note_Main_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
