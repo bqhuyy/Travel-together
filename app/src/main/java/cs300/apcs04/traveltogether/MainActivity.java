8ef0a6b83d6033fbbed1fdd7fd83465106c557e2
Dinh-Hieu Hoang <doncui1811@gmail.com>
2018-11-07 09:41:20 +0700

    add login

--- app/src/main/java/cs300/apcs04/traveltogether/MainActivity.java
+++ app/src/main/java/cs300/apcs04/traveltogether/MainActivity.java
@@ -1,7 +1,10 @@
 package cs300.apcs04.traveltogether;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        finish();
+    }
+
+    public void OnAccountButton(View view) {
+        Intent intent = new Intent(this, ManageAccountActivity.class);
+        startActivity(intent);
     }
 }
