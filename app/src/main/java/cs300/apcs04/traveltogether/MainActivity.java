de671faaac0b4795dbf9f192285e427dbb7d025c
bqhuyy <huynt.ptnk@gmail.com>
2018-11-19 00:17:59 +0700

    add navigation bar to main activity

--- app/src/main/java/cs300/apcs04/traveltogether/MainActivity.java
+++ app/src/main/java/cs300/apcs04/traveltogether/MainActivity.java
@@ -1,35 +1,58 @@
 package cs300.apcs04.traveltogether;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.firebase.ui.auth.ui.email.SignInActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private NavigationView mNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.main_drawer_layout);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mNavigationView = (NavigationView)findViewById(R.id.main_nav_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.main_nav_about:
+                        break;
+                    case R.id.main_nav_signout:
+                        FirebaseAuth.getInstance().signOut();
+//                        Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+//                        startActivity(intent);
+                        finish();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
-    public void onBackPressed() {
-        FirebaseAuth.getInstance().signOut();
-        finish();
-    }
-
-    public void OnAccountButton(View view) {
-        Intent intent = new Intent(this, ManageAccountActivity.class);
-        startActivity(intent);
-    }
-
-    public void OnSignOutButton(View view) {
-        FirebaseAuth.getInstance().signOut();
-        finish();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mActionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
