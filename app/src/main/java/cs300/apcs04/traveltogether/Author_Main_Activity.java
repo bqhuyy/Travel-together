02df56b46e4e3d0d179701e7d2cbbeb6ec091e17
ldtuanAPCS <ldtuan@apcs.vn>
2018-11-28 12:53:22 +0700

    add author section and add theme to note in manifest_file

---
+++ app/src/main/java/cs300/apcs04/traveltogether/Author_Main_Activity.java
@@ -0,0 +1,48 @@
+package cs300.apcs04.traveltogether;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class Author_Main_Activity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private SlideAdapter myadapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_author_main);
+        viewPager = (ViewPager) findViewById(R.id.AuthorViewPager);
+        myadapter = new SlideAdapter(this);
+        viewPager.setAdapter(myadapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            boolean lastpagechange = false;
+
+            @Override
+            public void onPageScrolled(int position, float v, int i1) {
+                int lastIdx = myadapter.getCount() - 1;
+                // End of intent
+                if (lastpagechange && position == lastIdx){
+                    Toast.makeText(Author_Main_Activity.this, "End of author section", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                int lastIdx = myadapter.getCount() - 1;
+                int currItem = viewPager.getCurrentItem();
+                if (currItem == lastIdx && state == 1){
+                    lastpagechange = true;
+                } else {
+                    lastpagechange = false;
+                }
+
+            }
+        });
+    }
+}
