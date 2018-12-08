92e8073383b702c21c8ef715463d79266f89e692
bqhuyy <huynt.ptnk@gmail.com>
2018-12-08 20:51:29 +0700

    fix bug show username

--- app/src/main/java/cs300/apcs04/traveltogether/GroupChatActivity.java
+++ app/src/main/java/cs300/apcs04/traveltogether/GroupChatActivity.java
@@ -1,6 +1,7 @@
 package cs300.apcs04.traveltogether;
 
 import android.content.Intent;
+import android.renderscript.Sampler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -20,6 +21,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.shrikanthravi.chatview.data.Message;
 import com.shrikanthravi.chatview.widget.ChatView;
 
@@ -28,7 +30,7 @@ import java.util.Iterator;
 
 public class GroupChatActivity extends AppCompatActivity {
     private static final int SIGN_IN_REQUEST_CODE = 1;
-    String groupChatID = "123", planID = "abcd"; // get from plan intent
+    String planID = "6cdec0fc-bb4f-4302-9c7f-5cf10454aeed"; // get from plan intent
     RelativeLayout activity_chat;
     FirebaseDatabase database;
     DatabaseReference messageRef, listOfMemberRef, userRef;
@@ -47,9 +49,8 @@ public class GroupChatActivity extends AppCompatActivity {
         planID = intent.getStringExtra("planID");
 
         database = FirebaseDatabase.getInstance();
-        messageRef = database.getReference("message/"+groupChatID);
+        messageRef = database.getReference("message/"+planID);
         listOfMemberRef = database.getReference("plan/"+planID+"/listOfMember");
-        userRef = database.getReference("user");
         // Read from the database
 
         chatView.setOnClickSendButtonListener(new ChatView.OnClickSendButtonListener() {
@@ -59,13 +60,44 @@ public class GroupChatActivity extends AppCompatActivity {
                         FirebaseAuth.getInstance().getCurrentUser().getUid()));
             }
         });
+        getData();
+    }
 
-        userRef.addChildEventListener(new ChildEventListener() {
+    public void getData() {
+        ChildEventListener childEventListener = new ChildEventListener() {
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String key = dataSnapshot.getKey();
-                if (userInfo.get(key) == "") {
-                    userInfo.put(key, dataSnapshot.child("name").getValue(String.class));
+            public void onChildAdded(DataSnapshot messageSnapshot, String s) {
+                final Iterator iterator = messageSnapshot.getChildren().iterator();
+                String userId;
+                while (iterator.hasNext()) {
+                    final Message message = new Message();
+                    message.setBody((String)((DataSnapshot)iterator.next()).getValue()); //message body
+                    message.setTime((String)(DateFormat.format("h:mm a, MMM d, yyyy",(Long)((DataSnapshot)iterator.next()).getValue())));
+                    userId = (String)((DataSnapshot)iterator.next()).getValue();
+                    if (FirebaseAuth.getInstance().getCurrentUser().getUid().equals(userId)) {
+                        message.setType(Message.RightSimpleMessage); //message type
+                    }
+                    else {
+                        userRef = database.getReference("user/"+userId);
+                        userRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot userSnapshot) {
+                                message.setUserName(userSnapshot.child("name").getValue(String.class));
+                                message.setType(Message.LeftSimpleMessage); //message type
+                                chatView.addMessage(message);
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+                    }
+
+//                  message.setUserName((String)((DataSnapshot)iterator.next()).getValue()); //sender name
+                    //sender icon
+
                 }
             }
 
@@ -88,144 +120,27 @@ public class GroupChatActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });
-
-
-        messageRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                if (dataSnapshot.exists() && (FirebaseAuth.getInstance().getCurrentUser() != null)) {
-                    DisplayMessages(dataSnapshot);
-                }
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                if (dataSnapshot.exists() && (FirebaseAuth.getInstance().getCurrentUser() != null)) {
-                    DisplayMessages(dataSnapshot);
-                }
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+        };
+        messageRef.addChildEventListener(childEventListener);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (FirebaseAuth.getInstance().getCurrentUser() == null)
-        {
-            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),SIGN_IN_REQUEST_CODE);
-        }
-        else
-        {
-            Snackbar.make(activity_chat,"Welcome "+FirebaseAuth.getInstance().getCurrentUser().getEmail(),Snackbar.LENGTH_SHORT).show();
-        }
-        userInfo = new HashMap<String, String>();
-        listOfMemberRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                if (dataSnapshot.exists() && (FirebaseAuth.getInstance().getCurrentUser() != null)) {
-                    userInfo.put(dataSnapshot.child("id").getValue(String.class),"");
-                }
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                if (dataSnapshot.exists() && (FirebaseAuth.getInstance().getCurrentUser() != null)) {
-                    userInfo.put(dataSnapshot.child("id").getValue(String.class),"");
-                }
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
     }
-
-    private void DisplayMessages(DataSnapshot dataSnapshot) {
-        final Iterator iterator = dataSnapshot.getChildren().iterator();
-        String userId;
-        while (iterator.hasNext()) {
-            final Message message = new Message();
-            message.setBody((String)((DataSnapshot)iterator.next()).getValue()); //message body
-            message.setTime((String)(DateFormat.format("h:mm a, MMM d, yyyy",(Long)((DataSnapshot)iterator.next()).getValue())));
-            userId = (String)((DataSnapshot)iterator.next()).getValue();
-            if (FirebaseAuth.getInstance().getCurrentUser().getUid().equals(userId)) {
-                message.setType(Message.RightSimpleMessage); //message type
-            }
-            else {
-                message.setUserName(userInfo.get(userId));
-                message.setType(Message.LeftSimpleMessage); //message type
-            }
-
-//            message.setUserName((String)((DataSnapshot)iterator.next()).getValue()); //sender name
-            //sender icon
-            chatView.addMessage(message);
-        }
-    }
-
-
-
+    
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case SIGN_IN_REQUEST_CODE:
-                if (resultCode == RESULT_OK) {
-                    Snackbar.make(activity_chat,"Successfully signed in. Welcome!", Snackbar.LENGTH_SHORT).show();
-                }
-                else {
-                    Snackbar.make(activity_chat,"We couldn't sign you in. Please try again later", Snackbar.LENGTH_SHORT).show();
-                    finish();
-                }
-                break;
-        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.chat_menu_sign_out:
-                AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Snackbar.make(activity_chat,"You have been signed out.",Snackbar.LENGTH_SHORT).show();
-                        finish();
-                    }
-                });
-                break;
-        }
         return true;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_chat,menu);
         return true;
     }
 }
